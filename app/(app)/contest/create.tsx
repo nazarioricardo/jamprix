244e58cf601ca0b258341f03aac3a43874e49f11
Ricardo Nazario <nazarioricardo@gmail.com>
2024-05-27 11:13:47 -0400

    Initial form

---
+++ app/(app)/contest/create.tsx
@@ -0,0 +1,41 @@
+import { Formik } from "formik";
+import { Button, StyleSheet, TextInput, View } from "react-native";
+
+function ContestCreate() {
+  return (
+    <Formik
+      initialValues={{ email: "" }}
+      onSubmit={(values) => console.log(values)}
+    >
+      {({ handleChange, handleBlur, handleSubmit, values }) => (
+        <View>
+          <TextInput
+            onChangeText={handleChange("email")}
+            onBlur={handleBlur("email")}
+            value={values.email}
+          />
+          <Button onPress={() => handleSubmit()} title="Submit" />
+        </View>
+      )}
+    </Formik>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 8,
+  },
+  errorText: {
+    color: "red",
+    marginBottom: 10,
+  },
+});
+
+export default ContestCreate;
--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,11 +1,26 @@
 import { Button, Text, View } from "react-native";
 import { useSession } from "../../providers/useSession";
+// import { supabase } from "../../supabase/initSupabase";
+import { useRouter } from "expo-router";
 
 function Home() {
+  const router = useRouter();
   const { signOut } = useSession();
+  const onPressCreatePrix = async () => {
+    router.push("contest/create");
+    router;
+    // try {
+    //   const response = await supabase.from("contest").insert({ name: "Prix" });
+    //   console.log(response);
+    // } catch (error) {
+    //   console.error(error);
+    // }
+  };
+
   return (
     <View>
       <Text>Testing</Text>
+      <Button title="Create Prix" onPress={onPressCreatePrix} />
       <Button title="Sign Out" onPress={signOut} />
     </View>
   );
