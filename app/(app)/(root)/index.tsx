8fcd5c9638b2f6108ea1a70c0def37da1085e647
Ricardo Nazario <nazarioricardo@gmail.com>
2024-06-29 20:22:26 -0400

    getting user channels

--- app/(app)/(root)/index.tsx
+++ app/(app)/(root)/index.tsx
@@ -1,11 +1,40 @@
+import { useEffect, useState } from "react";
 import { Button, Text, View } from "react-native";
 import { useRouter } from "expo-router";
 import { useSession } from "../../../providers/useSession";
 import { supabase } from "../../../supabase/initSupabase";
 
+type Channel = {
+  id: string;
+  title: string;
+  description: string;
+  created_at: string;
+};
+
+type Participant = {
+  user: string;
+  channel: Channel;
+};
+
 function Home() {
   const router = useRouter();
-  const { signOut, email } = useSession();
+  const { signOut, email, database } = useSession();
+  const [channels, setChannels] = useState<Channel[]>([]);
+  useEffect(() => {
+    supabase
+      .from("participants")
+      .select("*, channel (*)")
+      .eq("user", database?.id)
+      .then(({ data }) => {
+        console.log(data);
+        if (!data) {
+          setChannels([]);
+          return;
+        }
+
+        setChannels(data.map((partipant: Participant) => partipant.channel));
+      });
+  }, []);
 
   const onPressCreatePrix = async () => {
     router.push("contest/create");
@@ -14,6 +43,14 @@ function Home() {
   return (
     <View>
       <Text>{email}</Text>
+      {channels.map(({ id, title, description }) => {
+        return (
+          <View key={id}>
+            <Text>{title}</Text>
+            <Text>{description}</Text>
+          </View>
+        );
+      })}
       <Button title="Create Prix" onPress={onPressCreatePrix} />
       <Button
         title="Supabase Test"
