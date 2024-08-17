f2e4f4762472e6a4ba3a54e1f32faf8a59f101e6
Ricardo Nazario <nazarioricardo@gmail.com>
2024-08-17 11:27:50 -0400

    dark theme

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,4 +1,4 @@
-import { tamaguiConfig } from "@/tamagui.config";
+import { useColorScheme } from "react-native";
 import {
   DarkTheme,
   DefaultTheme,
@@ -6,50 +6,36 @@ import {
   ThemeProvider,
 } from "@react-navigation/native";
 import { Stack } from "expo-router";
-import { useColorScheme } from "react-native";
-import { TamaguiProvider, useTheme } from "tamagui";
 
-const JamDarkTheme: Theme = {
-  ...DarkTheme,
-  colors: {
-    ...DarkTheme.colors,
-    primary: tamaguiConfig.themes.dark.accentColor.val,
-    background: tamaguiConfig.themes.dark.background.val,
-  },
-};
-
-const JamLightTheme: Theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: tamaguiConfig.themes.light.accentColor.val,
-    background: tamaguiConfig.themes.light.background.val,
-  },
-};
+// const JamDarkTheme: Theme = {
+//   ...DarkTheme,
+//   colors: {
+//     ...DarkTheme.colors,
+//     primary: tamaguiConfig.themes.dark.accentColor.val,
+//     background: tamaguiConfig.themes.dark.background.val,
+//   },
+// };
 
 export default function AppLayout() {
   // const colorScheme = useColorScheme();
 
-  // <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
   // <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
   return (
-    <TamaguiProvider config={tamaguiConfig} defaultTheme={"dark"}>
-      <ThemeProvider value={JamDarkTheme}>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(root)" />
-          <Stack.Screen
-            name="login"
-            options={{ presentation: "modal", gestureEnabled: false }}
-          />
-          <Stack.Screen
-            name="profile"
-            options={{
-              presentation: "modal",
-              title: "Profile",
-            }}
-          />
-        </Stack>
-      </ThemeProvider>
-    </TamaguiProvider>
+    <ThemeProvider value={DarkTheme}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(root)" />
+        <Stack.Screen
+          name="login"
+          options={{ presentation: "modal", gestureEnabled: false }}
+        />
+        <Stack.Screen
+          name="profile"
+          options={{
+            presentation: "modal",
+            title: "Profile",
+          }}
+        />
+      </Stack>
+    </ThemeProvider>
   );
 }
