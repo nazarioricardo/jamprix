5e2a4072998e2e9c943d9c171474c8b4fb56a5cf
Ricardo Nazario <nazarioricardo@gmail.com>
2024-08-17 11:45:11 -0400

    removed white background

--- app/(app)/login.tsx
+++ app/(app)/login.tsx
@@ -38,7 +38,7 @@ function Login() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    // backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
