4bf38326e0a446c4dad88df73e1234b7b4515344
Ricardo Nazario <nazarioricardo@gmail.com>
2024-08-24 16:56:17 -0400

    profile view

--- app/(app)/profile.tsx
+++ app/(app)/profile.tsx
@@ -1,10 +1,11 @@
-import { Button, View } from "react-native";
+import { Button, View, Text, YStack, useTheme } from "tamagui";
 import { useRouter } from "expo-router";
 import { useSession } from "@/providers/useSession";
 
 function Profile() {
+  const theme = useTheme();
   const router = useRouter();
-  const { signOut } = useSession();
+  const { signOut, email, provider } = useSession();
 
   const onPressSignOut = () => {
     router.navigate("/");
@@ -14,9 +15,31 @@ function Profile() {
     }
   };
 
+  const onPressDismiss = () => {
+    router.dismiss();
+  };
+
   return (
     <View>
-      <Button title="Sign Out" onPress={onPressSignOut} />
+      <YStack
+        style={{
+          padding: 24,
+          paddingBottom: 48,
+          height: "100%",
+          justifyContent: "space-between",
+        }}
+      >
+        <YStack>
+          <Text>{provider}</Text>
+          <Text>{email}</Text>
+        </YStack>
+        <Button backgroundColor={"$color6"} onPress={onPressSignOut}>
+          Sign Out
+        </Button>
+        <Button backgroundColor={"$color6"} onPress={onPressDismiss}>
+          Dismiss
+        </Button>
+      </YStack>
     </View>
   );
 }
