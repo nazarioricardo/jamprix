2576b8e8c0483a3518746c52f024448302368f7a
Ricardo Nazario <nazarioricardo@gmail.com>
2025-09-13 13:21:59 -0400

    creating channel with events

--- app/(app)/(root)/channel/create.tsx
+++ app/(app)/(root)/channel/create.tsx
@@ -1,9 +1,10 @@
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
 import { Formik } from "formik";
-import { Input, Button, TextArea } from "tamagui";
+import { Input, Button, TextArea, Text } from "tamagui";
 import { router } from "expo-router";
 import { supabase } from "@/supabase/initSupabase";
 import { useSession } from "@/providers/useSession";
+import { useState } from "react";
 
 type ChannelFormValues = {
   title: string;
@@ -11,14 +12,15 @@ type ChannelFormValues = {
 };
 
 function ChannelCreate() {
+  // TODO: FORM VALIDATION
+  // TODO: Make atomic - create channel, create participants, create events
   const { session } = useSession();
 
-  const onSubmitChannel = async ({ title, description }: ChannelFormValues) => {
-    if (!session) {
-      return;
-    }
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-    const { data: channelData, error: channelError } = await supabase
+  const createChannel = async (title: string, description: string) => {
+    const { data, error } = await supabase
       .from("channels")
       .insert([
         {
@@ -29,30 +31,74 @@ function ChannelCreate() {
       .select()
       .single();
 
-    console.log("data", channelData);
-    if (channelError) {
-      console.error("Error creating channel:", channelError);
+    if (error) {
+      throw new Error(`Error posting channel: ${error.message}`);
+    }
+
+    return data;
+  };
+
+  const createParticipant = async (channelId: string, userId: string) => {
+    const { error } = await supabase.from("participants").insert([
+      {
+        channel: channelId,
+        user: userId,
+      },
+    ]);
+
+    if (error) {
+      throw new Error(`Error adding creator as participant: ${error.message}`);
+    }
+  };
+
+  const createEvents = async (channelId: string) => {
+    const { data: themes } = await supabase
+      .from("themes")
+      .select()
+      .order("random()")
+      .limit(3);
+
+    if (!themes || themes.length === 0) {
+      throw new Error("Error fetching themes");
+    }
+
+    const events = themes.map((theme) => ({
+      theme_id: theme.id,
+      channel_id: channelId,
+    }));
+
+    const { error } = await supabase.from("events").insert(events);
+    if (error) {
+      throw new Error(`Error creating events: ${error}`);
+    }
+  };
+
+  const onSubmitChannel = async ({ title, description }: ChannelFormValues) => {
+    if (!session) {
       return;
     }
 
-    const { error: participantError } = await supabase
-      .from("participants")
-      .insert([
-        {
-          channel: channelData.id,
-          user: session.user.id,
-        },
-      ]);
+    setLoading(true);
+    setErrorMessage("");
 
-    if (participantError) {
-      console.error("Error adding creator as participant:", participantError);
-      return;
-    }
+    try {
+      const channel = await createChannel(title, description);
 
-    if (router.canDismiss()) {
-      router.dismiss();
-    } else {
-      router.navigate("/");
+      if (!channel) {
+        throw new Error("Failed to create channel");
+      }
+
+      await createParticipant(channel.id, session.user.id);
+      await createEvents(channel.id);
+
+      router.canDismiss() ? router.dismiss() : router.navigate("/");
+    } catch (error) {
+      const message =
+        error instanceof Error ? error.message : "Something went wrong";
+
+      setErrorMessage(message);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -62,7 +108,15 @@ function ChannelCreate() {
       onSubmit={onSubmitChannel}
     >
       {({ handleChange, handleBlur, handleSubmit, values }) => (
-        <View>
+        <View
+          style={{
+            padding: 24,
+            paddingBottom: 48,
+            height: "100%",
+            justifyContent: "flex-start",
+            gap: 12,
+          }}
+        >
           <Input
             onChangeText={handleChange("title")}
             onBlur={handleBlur("title")}
@@ -75,28 +129,14 @@ function ChannelCreate() {
             value={values.description}
             placeholder="Description..."
           />
-          <Button onPress={() => handleSubmit()}>Submit</Button>
+          <Button disabled={loading} onPress={() => handleSubmit()}>
+            {loading ? "Creating..." : "Submit"}
+          </Button>
+          {errorMessage && <Text color={"red"}>{errorMessage}</Text>}
         </View>
       )}
     </Formik>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 16,
-  },
-  input: {
-    height: 40,
-    borderColor: "gray",
-    borderWidth: 1,
-    marginBottom: 10,
-    padding: 8,
-  },
-  errorText: {
-    color: "red",
-    marginBottom: 10,
-  },
-});
 
 export default ChannelCreate;
